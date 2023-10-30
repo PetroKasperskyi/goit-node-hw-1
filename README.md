6b85c56e0851ab9b89053e92a3d60a822dfb4b81
PetroKasperskyi <kaspersky28061993@gmail.com>
2023-10-30 14:18:52 +0200

    done

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # goit-node-hw-1
+
+1. https://monosnap.com/file/x3fAkdbGjTHhsVf0OU585wqBivgSfY
+2. https://monosnap.com/file/JNaNRaie7kab0NlsYuFYKWceIMH7Nf
+3. https://monosnap.com/file/sxPIspF0VAXv1J7wIFHqyPxjAB8qrs
+4. https://monosnap.com/file/VJ9v9didoTHTtixyXXKA1Wy7VXUzt9
--- contacts.js
+++ contacts.js
@@ -2,18 +2,23 @@ const fs = require("node:fs/promises");
 
 const path = require("node:path");
 
+const crypto = require("node:crypto");
 
 const contactsPath = path.join(__dirname, "db", "contacts.json");
+
 
 async function readContactsList() {
     try {
         const data = await fs.readFile(contactsPath, { encoding: "utf-8" })
-        return JSON.parse(data)
-    } catch (error) {
-        throw new Error(error.massege)
+      return JSON.parse(data);
     }
+    catch (error) {
+        throw new Error(error.massege)
+  }
+ 
 };
-
+ 
+readContactsList();
 async function writeContactsList(contacts) {
   try {
     return await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
@@ -22,19 +27,55 @@ async function writeContactsList(contacts) {
   }
 }
 
-function listContacts() {
- 
+ async function listContacts() {
+  try {
+    return await readContactsList();
+  } catch (error) {
+    throw error;
+  }
 };
 
-function getContactById(contactId) {
-  // ...твій код. Повертає об'єкт контакту з таким id. Повертає null, якщо контакт з таким id не знайдений.
+async function getContactById(contactId) {
+  try {
+    const allContacts = await readContactsList();
+    const contact = allContacts.find((contact) => contact.id === contactId);
+    return contact || null;
+  } catch (error) {
+    throw error;
+  }
 }
 
-function removeContact(contactId) {
-  // ...твій код. Повертає об'єкт видаленого контакту. Повертає null, якщо контакт з таким id не знайдений.
-}
 
-function addContact(name, email, phone) {
-  // ...твій код. Повертає об'єкт доданого контакту. 
-}
+async function removeContact(contactId) {
+  try{
+  const allContacts = await readContactsList();
+  const index = allContacts.findIndex((contact) => contact.id === contactId);
+  if (index === -1) {
+    return null;
+  }
+   const [result] = allContacts.splice(index, 1);
+    await writeContactsList(allContacts);
+    return result;
+  } catch (error) {
+    throw error;
+  }
 
+}
+async function addContact(name, email, phone) {
+  try {
+    const allContacts = await readContactsList();
+    const newContact = { id: crypto.randomUUID(), name, email, phone };
+    allContacts.push(newContact);
+     await writeContactsList(allContacts);
+    return newContact || null;
+  } catch (error) {
+    throw error;
+    }
+}
+ 
+module.exports = {
+  listContacts,
+  getContactById,
+  removeContact,
+   addContact
+}
